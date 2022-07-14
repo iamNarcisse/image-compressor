50bd8dcd1e4d2b6df3042dd4823031b263432891
Narcisse Egonu <mechikaegon@gmail.com>
2022-07-14 22:36:07 +0100

    Implement oxipng

--- src/compressor.rs
+++ src/compressor.rs
@@ -1,5 +1,7 @@
 use oxipng::optimize;
 use oxipng::{InFile, IndexSet, OutFile, PngError};
+use std::path::Path;
+use std::path::PathBuf;
 
 extern crate oxipng;
 
@@ -12,17 +14,19 @@ impl Compressor {
         Compressor {}
     }
 
-    pub fn compress(&self, input: &InFile) -> PngResult<()> {
-        let (output, opts) = get_opts(input);
-        let result = optimize(&input, &output, &opts);
+    pub fn compress(&self, input: &str) -> PngResult<()> {
+        let input = PathBuf::from(input);
+        let (output, opts) = get_opts(&input);
+        let result = optimize(&InFile::Path(input), &output, &opts);
+
         match result {
-            Ok(data) => Ok(()),
-            Err(e) => panic!("{}", e),
+            Ok(data) => Ok(data),
+            Err(e) => Err(e),
         }
     }
 }
 
-fn get_opts(input: &InFile) -> (OutFile, oxipng::Options) {
+fn get_opts(input: &Path) -> (OutFile, oxipng::Options) {
     let mut options = oxipng::Options {
         force: true,
         ..Default::default()
@@ -31,5 +35,19 @@ fn get_opts(input: &InFile) -> (OutFile, oxipng::Options) {
     filter.insert(0);
     options.filter = filter;
 
-    (OutFile::Path(None), options)
+    (
+        OutFile::Path(Some(input.with_extension("out.png"))),
+        options,
+    )
+}
+
+#[test]
+fn test_compress() {
+    let engine = Compressor::new();
+    let result = engine.compress("compress.png");
+
+    match result {
+        Ok(_data) => (println!("Success!")),
+        Err(e) => panic!("Error {}", e),
+    }
 }
