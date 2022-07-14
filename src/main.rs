47ce47eb748c79a680539190b1e6d0aa5f95bc0b
Narcisse Egonu <mechikaegon@gmail.com>
2022-07-14 23:33:32 +0100

    Route (WIP)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
+use rocket::form::{Form, FromForm};
+use rocket::fs::TempFile;
 pub mod compressor;
 
 #[macro_use]
 extern crate rocket;
+
+#[derive(FromForm)]
+struct Upload<'r> {
+    save: bool,
+    file: TempFile<'r>,
+}
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[post("/")]
-fn compress() -> &'static str {
-    "Hello, compress!"
+#[post("/", data = "<upload>")]
+fn compress(upload: Form<Upload<'_>>) -> Result<(), &'static str> {
+    let engine = compressor::Compressor::new();
+    let response = engine.compress("compress.png");
+    match response {
+        Ok(data) => Ok(data),
+        Err(e) => Err("Failed to compress image "),
+    }
 }
 
 #[launch]
